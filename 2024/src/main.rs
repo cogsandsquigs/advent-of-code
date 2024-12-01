fd32e561e589f8bfe4031e70c5590bf7fc717bf5
Ian Pratt <ianjdpratt@gmail.com>
2024-12-01 13:36:20 +0000

    prep for something more

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,5 +1,10 @@
+use std::{collections::HashMap, sync::LazyLock};
+
 mod day_1;
 mod day_2;
+
+static SOLUTIONS: LazyLock<HashMap<usize, fn() -> ()>> =
+    LazyLock::new(|| HashMap::from([(1, day_1::run as fn()), (2, day_2::run as fn())]));
 
 // TODO: add CLI for selecting days
 fn main() {
