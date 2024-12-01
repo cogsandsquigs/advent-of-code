bcb2a4d9e845db40ce56be8de3068db65f0b4cec
Ian Pratt <ianjdpratt@gmail.com>
2024-12-01 13:32:23 +0000

    prep for day 2

---
+++ 2024/src/day_2.rs
@@ -0,0 +1,16 @@
+use utils::macros::solution;
+
+#[solution(day = 1, part = 1)]
+pub fn part_1(input: &str) -> usize {
+    todo!()
+}
+
+#[solution(day = 1, part = 2)]
+pub fn part_2(input: &str) -> usize {
+    todo!()
+}
+
+pub fn run() {
+    part_1();
+    part_2();
+}
