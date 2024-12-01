80c670aecdb91eaa5069de3b3df551c9ca7f5dc2
Ian Pratt <ianjdpratt@gmail.com>
2024-12-01 13:30:13 +0000

    do day 1

--- 2024/src/day_1.rs
+++ 2024/src/day_1.rs
@@ -1,13 +1,36 @@
-use utils::macros::solution;
+use std::collections::{HashMap, HashSet};
+
+use utils::{itertools::Itertools, macros::solution};
 
 #[solution(day = 1, part = 1)]
 pub fn part_1(input: &str) -> usize {
-    todo!()
+    let (mut left, mut right): (Vec<_>, Vec<_>) = input
+        .lines()
+        .map(|x| x.split_whitespace())
+        .map(|mut x| (x.next().unwrap(), x.next().unwrap()))
+        .map(|(a, b)| (a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap()))
+        .unzip();
+
+    left.sort();
+    right.sort();
+
+    left.iter().zip(right).map(|(a, b)| a.abs_diff(b)).sum()
 }
 
 #[solution(day = 1, part = 2)]
 pub fn part_2(input: &str) -> usize {
-    todo!()
+    let (mut left, mut right): (Vec<_>, Vec<_>) = input
+        .lines()
+        .map(|x| x.split_whitespace())
+        .map(|mut x| (x.next().unwrap(), x.next().unwrap()))
+        .map(|(a, b)| (a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap()))
+        .unzip();
+
+    let right_set = right.iter().counts_by(|x| x);
+
+    left.iter()
+        .map(|x| x * right_set.get(x).unwrap_or(&0))
+        .sum()
 }
 
 pub fn run() {
