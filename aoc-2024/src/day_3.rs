82acaa77de95684fb7ffc6871aea1e9ce5d20216
Ian Pratt <ianjdpratt@gmail.com>
2024-12-03 11:09:48 +0000

    functional :3

--- aoc-2024/src/day_3.rs
+++ aoc-2024/src/day_3.rs
@@ -7,13 +7,7 @@ fn part_1(input: &str) -> usize {
     r.find_iter(input)
         .map(|m| &m.as_str()[4..m.len() - 1])
         .map(|s| s.split(','))
-        .map(|mut i| {
-            (
-                i.next().unwrap().parse::<usize>().unwrap(),
-                i.next().unwrap().parse::<usize>().unwrap(),
-            )
-        })
-        .map(|(a, b)| a * b)
+        .map(|i| i.map(|v| v.parse::<usize>().unwrap()).product::<usize>())
         .sum()
 }
 
@@ -21,22 +15,21 @@ fn part_1(input: &str) -> usize {
 fn part_2(input: &str) -> usize {
     let r = Regex::new(r"(mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\))").unwrap();
 
-    let mut s = 0;
-    let mut do_mul = true;
-
-    for m in r.find_iter(input) {
-        match m.as_str() {
-            "do()" => do_mul = true,
-            "don't()" => do_mul = false,
-            v => {
-                if do_mul {
-                    let mut i = v[4..v.len() - 1].split(',');
-                    s += i.next().unwrap().parse::<usize>().unwrap()
-                        * i.next().unwrap().parse::<usize>().unwrap();
-                }
-            }
-        }
-    }
-
-    s
+    r.find_iter(input)
+        .fold((0, true), |(s, do_mul), m| match m.as_str() {
+            "do()" => (s, true),
+            "don't()" => (s, false),
+            v => (
+                s + if do_mul {
+                    v[4..v.len() - 1]
+                        .split(',')
+                        .map(|v| v.parse::<usize>().unwrap())
+                        .product::<usize>()
+                } else {
+                    0
+                },
+                do_mul,
+            ),
+        })
+        .0
 }
